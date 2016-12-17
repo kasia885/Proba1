872d4d1b38873612020456ade33ee6c8c0f32259
kasia885 <kasia.arciszewska96@wp.pl>
2016-12-17 11:31:17 +0100

    Add files via upload

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new WlasnaLista<int>();
+            lista.Add(5);
+
+            foreach (var i in lista)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+
+    class WlasnaLista<T> : System.Collections.CollectionBase
+    {
+        public void Add(T element)
+        {
+            List.Add(element);
+        }
+    }
+}
